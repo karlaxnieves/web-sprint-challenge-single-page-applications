5c27f24769095e72686d5a3ee5c9997a893015a1
karlaxnieves <78055815+karlaxnieves@users.noreply.github.com>
2021-04-23 08:42:53 -0700

    created components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import * as yup from 'yup';
+import Form from './Form';
+import schema from './formSchema';
+import Home from './Home'
+import Confirmation from './Confirmation'
+import { Route, withRouter, Switch } from 'react-router-dom'
+
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+
     </>
   );
 };
-export default App;
+
+
+
+export default withRouter(App);
